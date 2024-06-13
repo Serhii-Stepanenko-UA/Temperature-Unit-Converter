2d82636f7c097786fa3abfdc5230e8f8478ca04d
Pulsar UA <sergivstep@gmail.com>
2024-06-14 01:20:13 +0300

    Add convert Celsius to Fahrenheit

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,13 +3,19 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter temperature unit App.");
-        double c = 36.6;
-        double f = 97.9;
+        double c = -40.0;
+        double f = -40.0;
         double tc = convFahrenheitToCelsius(f);
         System.out.println(f + " degrees Fahrenheit is " + tc + " degrees Celsius");
+        double tf = convCelsiusToFahrenheit(c);
+        System.out.println(c + " degrees Celsius is " + tf + " degrees Fahrenheit");
     }
 
     private static double convFahrenheitToCelsius(double f) {
-        return (f - 32) * 5 / 9;
+        return (f - 32) * ((double) 5 / 9);
+    }
+
+    private static double convCelsiusToFahrenheit(double c) {
+        return  c * 1.8 + 32;
     }
 }
